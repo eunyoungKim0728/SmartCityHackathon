6a36063c093ba89786f2a3056dbfb6690a5b395e
hyewon <everhyewonlee@github.com>
2023-05-03 23:35:18 -0400

    add medlist html

--- smartcity/.prescriptxn/start.py
+++ smartcity/.prescriptxn/start.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +10,11 @@ def home():
 @app.route('/register')
 def register():
     return render_template("register.html")
+
+@app.route('/medlist')
+def medlist():
+    return render_template("medlist.html")
+
 
 def main():
     app.run(host='127.0.0.1', debug=True, port=8000)
